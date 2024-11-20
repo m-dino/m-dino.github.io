627365ee4d31d2a37d7b2700b8f862648b4094c3
m-dino <annemae154@gmail.com>
2024-11-20 21:10:57 +0800

    script.js

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-// Initialize Swiper
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
+var swiper = new Swiper('.swiper-container', {
     loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-});
-
-// Event listeners for "About Me" buttons
-var aboutButtons = document.querySelectorAll(".aboutMe");
-
-aboutButtons.forEach(function(button) {
-  button.addEventListener("click", function() {
-    window.location.href = "about.html"; // Redirect to about page (create about.html)
-  });
-});
-
-// Event listeners for "Hire Me" buttons
-var hireButtons = document.querySelectorAll(".hireMe");
-
-hireButtons.forEach(function(button) {
-  button.addEventListener("click", function() {
-    window.location.href = "hire.html"; // Redirect to hire page (create hire.html)
-  });
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
 });
